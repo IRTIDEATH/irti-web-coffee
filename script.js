6aa5d13f1c77e554b9478a2f8197f0ec2a2b593b
IRTIDEATH <enderjhass@gmail.com>
2024-05-04 17:20:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+
+// toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button'); 
+
+document.addEventListener('click', function(e) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.targer)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if(!sb.contains(e.target) && !searchForm.contains(e.targer)) {
+        searchForm.classList.remove('active');
+    }
+})
